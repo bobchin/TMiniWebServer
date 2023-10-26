382b26fc3e96cf8572020f708c4453b173a1438f
kazunobu <k-ichihashi@hokkai-v.co.jp>
2023-10-26 14:32:41 +0900

    ・固定IPを使えるようにした ・リファクタリング

--- boot.py
+++ boot.py
@@ -1,31 +1,26 @@
-# boot.py -- run on boot-up
-import network
-import os
 import sys
+import network
+from time import sleep
 
-# Replace the following with your WIFI Credentials
-SSID = "YOUR_SSID"
-SSID_PASSWORD = "YOUR_SSID_PASSWORD"
+args = ['IP', 'SUB', 'GW', 'DNS', 'SSID', 'PASS']
 try:
     with open('laninfo.txt', 'rt') as f:
-        info = f.readlines()
-
-        SSID = info[0]
-        SSID_PASSWORD = info[1]
+        args = list(map(lambda s: s.rstrip(), f.readlines()))
+        del args[6:]
 except:
     print("Failed LAN Configuration.")
-    sys.exit()
+    sys.exit(1)
 
+print("Connecting to network...")
+wlan = network.WLAN(network.STA_IF)
+if not wlan.isconnected():
+    wlan.active(True)
+    wlan.connect(args[4], args[5])
+    wlan.ifconfig(tuple(args[0:4]))
 
-def do_connect():
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(SSID, SSID_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('Connected! Network config:', sta_if.ifconfig())
-    
-print("Connecting to your wifi...")
-do_connect()
+    while not wlan.isconnected():
+        print("waiting for connection...")
+        sleep(1)
+
+print('Connected!')
+print('####################\nIP: %s\nSUBNET: %s\nGW: %s\nDNS: %s\n####################\n' % wlan.ifconfig())
