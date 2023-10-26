31718cdbbc71cb440bde97e18e5b83dcd1729cb3
kazunobu <k-ichihashi@hokkai-v.co.jp>
2023-10-26 14:50:21 +0900

    ・LED点滅ロジック追加 ・Webサーバ以外に処理をする場合の考慮

--- main.py
+++ main.py
@@ -2,25 +2,47 @@ from TMiniWebServer import TMiniWebServer
 
 import uasyncio as asyncio
 import gc
-import sys
 
 import sample_basic
 import sample_restapi
 import sample_websocket
 
+from machine import Pin
+import time
 
-## Webサーバーの開始と定期的な掃除
-async def main_task():
+## LED
+led = Pin('LED', Pin.OUT)
+
+## Webサーバ
+async def webserver():
     webserver = TMiniWebServer()
     await webserver.start()
     print('TMiniWebServer started.')
 
+## LEDの定期点滅
+async def blink_led():
+    WAIT_SEC = 0.5
+    while True:
+        led.on()
+        await asyncio.sleep(WAIT_SEC)
+        led.off()
+        await asyncio.sleep(WAIT_SEC)
+
+## ガベージコレクション
+async def garbage_collection():
     while True:
         print(f'free memory: {gc.mem_free()}')
         await asyncio.sleep(60)
         gc.collect()
 
+## メイン
+async def main():
+    sv_task = asyncio.create_task(webserver())
+    led_task = asyncio.create_task(blink_led())
+    gc_task = asyncio.create_task(garbage_collection())
+    await sv_task
+    await led_task
+    await gc_task
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main_task())
-
+loop.run_until_complete(main())
